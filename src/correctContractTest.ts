8062d3b13c79611163a416c87d5208caff862d30
Ken Liao <kenliao94@gmail.com>
2023-10-30 21:25:39 +0800

    wrapped up skeleton code

--- src/correctContractTest.ts
+++ src/correctContractTest.ts
@@ -4,20 +4,39 @@ const exec = util.promisify(
 	(require('node:child_process') as typeof import('node:child_process')).exec
 )
 
+const DEFAULT_ITERATION_TIME = 3
+
 async function correctContractTest(githubUrl: string, runCommand: string, testFilePath: string) {
-    let file_content = ''
-    try {
-        file_content = await fileUtils.readFile(testFilePath, 'utf-8');
-        console.log(file_content);
-      } catch (error) {
-        console.error('Error reading file:', error);
-        return
-      }
-      console.log(`github url: ${githubUrl}`)
-      console.log(`running command ${runCommand}`)
-      const output = await exec(runCommand)
-      console.log(output.stdout)
+    let testRunSuccessfully = false
+    let iteration = 0
+    while (iteration < DEFAULT_ITERATION_TIME && !testRunSuccessfully) {
+        let fileContent = ''
+        try {
+            fileContent = await fileUtils.readFile(testFilePath, 'utf-8');
+        } catch (error) {
+            console.error('Error reading file:', error);
+            return
+        }
+        console.log(`running command ${runCommand}`)
+        const output = await exec(runCommand)
+        testRunSuccessfully = doesTestRunSuccessfully(output.stdout)
+        if (!testRunSuccessfully) {
+            const newFileContent = await correctTest(githubUrl, fileContent)
+            fileUtils.modifyFile(testFilePath, newFileContent)
+        } else {
+            break
+        }
+        iteration++
+    }
 }
+
+function doesTestRunSuccessfully(stdout: string):boolean {
+    return false
+}
+
+async function correctTest(githubUrl: string, file_content: string): Promise<string> {
+    return ''
+} 
 
 export {}
 exports.correctContractTest = correctContractTest
